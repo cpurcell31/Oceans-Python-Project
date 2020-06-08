7efed105d5e504adefaf385e8faa23a1b4cf56ae
cpurcell31 <cpurcell@uvic.ca>
2020-06-07 18:12:23 -0700

    updated gui to object oriented style

--- o2gui.py
+++ o2gui.py
@@ -2,67 +2,88 @@ import tkinter as tk
 import oceans2 as o2
 
 
+class OceanGui:
+
+    def __init__(self, *args, **kwargs):
+        self.window = tk.Tk()
+        self.frame_search = SearchFrame(self.window)
+        self.frame_download = DownloadFrame(self.window)
+        self.__pack_download_widgets__()
+        self.__pack_search_widgets__()
+        self.window.mainloop()
+
+    def __pack_download_widgets__(self):
+        self.frame_download.label_download.grid(row=0, column=0, padx=5)
+        self.frame_download.frame.grid(row=1, column=0, padx=5, sticky='w')
+
+    def __pack_search_widgets__(self):
+        self.frame_search.label_search.grid(row=0, column=0, sticky='w')
+
+        self.frame_search.loc_name_lbl.grid(row=1, column=0, padx=5, sticky='w')
+        self.frame_search.loc_name_entry.grid(row=1, column=1, padx=5, sticky='w')
+
+        self.frame_search.loc_code_lbl.grid(row=2, column=0, padx=5, sticky='w')
+        self.frame_search.loc_code_entry.grid(row=2, column=1, padx=5, sticky='w')
+
+        self.frame_search.dev_cat_lbl.grid(row=3, column=0, padx=5, sticky='w')
+        self.frame_search.dev_cat_entry.grid(row=3, column=1, padx=5, sticky='w')
+
+        self.frame_search.dev_code_lbl.grid(row=4, column=0, padx=5, sticky='w')
+        self.frame_search.dev_code_entry.grid(row=4, column=1, padx=5, sticky='w')
+
+        self.frame_search.find_product_check.grid(row=1, column=3, padx=10, sticky='w')
+        self.frame_search.date_check.grid(row=2, column=3, padx=10, sticky='w')
+
+        self.frame_search.search_button.grid(row=5, column=1)
+
+        self.frame_search.frame.grid(row=0, column=0, padx=5, sticky='w')
+
+
+class SearchFrame:
+
+    def __init__(self, window):
+        self.loc_name = ""
+        self.loc_code = ""
+        self.dev_cat = ""
+        self.dev_code = ""
+        self.product_on = 0
+        self.date_on = 0
+
+        self.frame = tk.Frame()
+
+        self.loc_name_entry = tk.Entry(master=self.frame)
+        self.loc_code_entry = tk.Entry(master=self.frame)
+        self.dev_cat_entry = tk.Entry(master=self.frame)
+        self.dev_code_entry = tk.Entry(master=self.frame)
+
+        self.label_search = tk.Label(master=self.frame, text="ONC Search")
+        self.loc_name_lbl = tk.Label(master=self.frame, text="Location Name")
+        self.loc_code_lbl = tk.Label(master=self.frame, text="Location Code")
+        self.dev_cat_lbl = tk.Label(master=self.frame, text="Device Category Code")
+        self.dev_code_lbl = tk.Label(master=self.frame, text="Device Code")
+
+        self.find_product_check = tk.Checkbutton(master=self.frame, text="Find Data Products", variable=self.product_on)
+        self.date_check = tk.Checkbutton(master=self.frame, text="Find Deployment Dates", variable=self.date_on)
+        self.search_button = tk.Button(master=self.frame, text="Search", command=self.handle_search)
+
+    def handle_search(self):
+        self.loc_name = self.loc_name_entry.get()
+        self.loc_code = self.loc_code_entry.get()
+        self.dev_cat = self.dev_cat_entry.get()
+        self.dev_code = self.dev_code_entry.get()
+
+
+class DownloadFrame:
+
+    def __init__(self, window):
+        self.frame = tk.Frame()
+        self.label_download = tk.Label(master=self.frame, text='ONC Download')
+
+
 def main():
 
-    window = tk.Tk()
-    frame_search = setup_search_frame(window)
-    frame_download = setup_download_frame(window)
-    search_widgets = frame_search.winfo_children()
-    download_widgets = frame_download.winfo_children()
-    window.mainloop()
+    gui = OceanGui()
     return
-
-
-def setup_search_frame(window):
-    frame_search = tk.Frame()
-
-    label_search = tk.Label(master=frame_search, text="ONC Search")
-    label_search.grid(row=0, column=0, sticky='w')
-
-    loc_name_lbl = tk.Label(master=frame_search, text="Location Name")
-    loc_code_lbl = tk.Label(master=frame_search, text="Location Code")
-    dev_cat_lbl = tk.Label(master=frame_search, text="Device Category Code")
-    dev_code_lbl = tk.Label(master=frame_search, text="Device Code")
-
-    loc_name_entry = tk.Entry(master=frame_search)
-    loc_code_entry = tk.Entry(master=frame_search)
-    dev_cat_entry = tk.Entry(master=frame_search)
-    dev_code_entry = tk.Entry(master=frame_search)
-
-    find_product_check = tk.Checkbutton(master=frame_search, text="Find Data Products")
-    date_check = tk.Checkbutton(master=frame_search, text="Find Deployment Dates")
-
-    search_button = tk.Button(master=frame_search, text="Search")
-
-    loc_name_lbl.grid(row=1, column=0, padx=5, sticky='w')
-    loc_name_entry.grid(row=1, column=1, padx=5, sticky='w')
-
-    loc_code_lbl.grid(row=2, column=0, padx=5, sticky='w')
-    loc_code_entry.grid(row=2, column=1, padx=5, sticky='w')
-
-    dev_cat_lbl.grid(row=3, column=0, padx=5, sticky='w')
-    dev_cat_entry.grid(row=3, column=1, padx=5, sticky='w')
-
-    dev_code_lbl.grid(row=4, column=0, padx=5, sticky='w')
-    dev_code_entry.grid(row=4, column=1, padx=5, sticky='w')
-
-    find_product_check.grid(row=1, column=3, padx=10, sticky='w')
-    date_check.grid(row=2, column=3, padx=10, sticky='w')
-
-    search_button.grid(row=5, column=1)
-
-    frame_search.grid(row=0, column=0, padx=5, sticky='w')
-    return frame_search
-
-
-def setup_download_frame(window):
-    frame_download = tk.Frame()
-
-    label_download = tk.Label(master=frame_download, text='ONC Download')
-    label_download.grid(row=0, column=0, sticky='w')
-
-    frame_download.grid(row=1, column=0, padx=5, sticky='w')
-    return frame_download
 
 
 if __name__ == "__main__":
